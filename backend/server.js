43c57595e9c4993da462f88e73b6138aba7d4124
ujjaini-das <ujjainidas737@gmail.com>
2026-09-24 22:40:33 +0530

    testing done

--- backend/server.js
+++ backend/server.js
@@ -15,6 +15,8 @@ const connectDB = require("./src/config/db");
 
 connectDB();
 
-app.listen(5000, () => {
-    console.log("server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`server running on port ${PORT}`);
 });
